94ab79fc4eb4b383433a47d6adad2c9575ff3c99
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2025-11-16 14:33:28 +0530

    Implement basic addition and subtraction in nums.py

---
+++ nums.py
@@ -0,0 +1,7 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+add_result = num1 + num2
+sub_result = num1 - num2
+
+print("Addition =", add_result)
+print("Subtraction =", sub_result)
